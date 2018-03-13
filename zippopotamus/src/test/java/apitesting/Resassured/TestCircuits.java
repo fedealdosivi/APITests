d28db65e241204ef2069ba11f09e19a3d23da737
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-03-13 17:45:51 -0300

    changes

--- zippopotamus/src/test/java/apitesting/Resassured/TestCircuits.java
+++ zippopotamus/src/test/java/apitesting/Resassured/TestCircuits.java
@@ -33,4 +33,41 @@ public class TestCircuits {
                 and().
                 header("Content-Length",equalTo("4551"));
     }
+
+    /**
+     * Test with query param
+     */
+    @Test
+    public void test_Md5CheckSumForTest_ShouldBe098f6bcd4621d373cade4e832627b4f6() {
+
+        String originalText = "test";
+        String expectedMd5CheckSum = "098f6bcd4621d373cade4e832627b4f6";
+
+        given().
+                param("text",originalText).
+                when().
+                get("http://md5.jsontest.com").
+                then().
+                assertThat().
+                body("md5",equalTo(expectedMd5CheckSum));
+    }
+
+    /**
+     * Test with path param
+     */
+    @Test
+    public void test_NumberOfCircuits_ShouldBe20_Parameterized() {
+
+        String season = "2017";
+        int numberOfRaces = 20;
+
+        given().
+                pathParam("raceSeason",season).
+                when().
+                get("http://ergast.com/api/f1/{raceSeason}/circuits.json").
+                then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits.circuitId",hasSize(numberOfRaces));
+    }
+
 }
